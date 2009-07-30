2cc8e31bb2e9f44119cb7e54ae31754671583acd
unknown <Garth@.(none)>
2009-07-30 08:09:32 -0400

    CLink test.

--- src/clojuratica/CLink.clj
+++ src/clojuratica/CLink.clj
@@ -1,1 +1,18 @@
-(ns clojuratica.CLink)
+(ns clojuratica.CLink
+  (:gen-class
+   :methods [#^{:static true} [onLoadClass [com.wolfram.jlink.KernelLink] void]
+                              [kl [] com.wolfram.jlink.KernelLink]]
+   :init init
+   :constructors {[] []}
+   :state state)
+  (:import [com.wolfram.jlink])
+  (:require [clojuratica.core] [clojuratica.lib]))
+
+(defn -init []
+  [[] (ref nil)])
+
+(defn -onLoadClass [this kernel-link]
+  (dosync (ref-set (.state this) kernel-link)))
+
+(defn -kl [this]
+  @(.state this))
